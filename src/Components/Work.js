15b834f9f1f5c913b6ce1f1f9687fc7da842434e
Vab31 <97352743+Vab31@users.noreply.github.com>
2023-05-11 00:45:19 +0530

    work section update

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -36,146 +36,83 @@ const Work = () => {
       <section id="works" className="container">
         <ul className="portfolio-items col-4">
           <li className="portfolio-item apps">
-            <div className="item-inner">
-              <img
-                src="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
-                alt=""
-              />
-              <h5>Lorem ipsum dolor sit amet</h5>
-              <div className="layer">
-                <a
-                  className="preview view"
-                  href="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
-                >
-                  <i className="fa fa-eye" />
-                </a>
-              </div>
-            </div>
+            
           </li>
           {/*/.portfolio-item*/}
           <li className="portfolio-item logo apps">
             <div className="item-inner">
-              <img
-                src="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
+            <img
+                src="https://i.ibb.co/9NBfyZm/Screenshot.png"
                 alt=""
               />
-              <h5>Lorem ipsum dolor sit amet</h5>
-              <div className="layer">
-                <a
-                  className="preview view"
-                  href="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
-                >
-                  <i className="fa fa-eye" />
-                </a>
-              </div>
+              <h5>Get code from <a href="https://youtu.be/s22I8dswAnY"> youtube description</a></h5>
+              
             </div>
           </li>
           {/*/.portfolio-item*/}
           <li className="portfolio-item logo web">
             <div className="item-inner">
               <img
-                src="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
+                src="https://i.ibb.co/n0w59vK/Screenshot-110.png"
                 alt=""
               />
-              <h5>Lorem ipsum dolor sit amet</h5>
-              <div className="layer">
-                <a
-                  className="preview view"
-                  href="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
-                >
-                  <i className="fa fa-eye" />
-                </a>
-              </div>
+               <h5>Get code from <a href="https://youtu.be/yKtycp-jQWU"> youtube description</a></h5>
+              
             </div>
           </li>
           {/*/.portfolio-item*/}
           <li className="portfolio-item logo web apps">
             <div className="item-inner">
               <img
-                src="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
+                src="https://i.ibb.co/vYmrckh/Screenshot-111.png"
                 alt=""
               />
-              <h5>Lorem ipsum dolor sit amet</h5>
-              <div className="layer">
-                <a
-                  className="preview view"
-                  href="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
-                >
-                  <i className="fa fa-eye" />
-                </a>
-              </div>
+               <h5>Get code from <a href="https://youtu.be/kQ3sSawjp4s"> youtube description</a></h5>
+              
             </div>
           </li>
           {/*/.portfolio-item*/}
           <li className="portfolio-item apps web">
             <div className="item-inner">
               <img
-                src="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
+                src="https://i.ibb.co/jRPzB7b/Screenshot-112.png"
                 alt=""
               />
-              <h5>Lorem ipsum dolor sit amet</h5>
-              <div className="layer">
-                <a
-                  className="preview view"
-                  href="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
-                >
-                  <i className="fa fa-eye" />
-                </a>
-              </div>
+            <h5>Get code from <a href="https://youtu.be/z9A7mJDiaZk"> youtube description</a></h5>
+               
             </div>
           </li>
           {/*/.portfolio-item*/}
           <li className="portfolio-item apps">
             <div className="item-inner">
               <img
-                src="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
+                src="https://i.ibb.co/TtpCzjm/Screenshot-113.png"
                 alt=""
               />
-              <h5>Lorem ipsum dolor sit amet</h5>
-              <div className="layer">
-                <a
-                  className="preview view"
-                  href="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
-                >
-                  <i className="fa fa-eye" />
-                </a>
-              </div>
+               <h5>Get code from <a href="https://youtu.be/yja-5ZbPIuE"> youtube description</a></h5>
+              
             </div>
           </li>
           {/*/.portfolio-item*/}
           <li className="portfolio-item web">
             <div className="item-inner">
               <img
-                src="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
+                src="https://i.ibb.co/n1WP513/Screenshot-115.png"
                 alt=""
               />
-              <h5>Lorem ipsum dolor sit amet</h5>
-              <div className="layer">
-                <a
-                  className="preview view"
-                  href="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
-                >
-                  <i className="fa fa-eye" />
-                </a>
-              </div>
+               <h5>Get code from <a href="https://youtu.be/wix9qo8oWx8"> youtube description</a></h5>
+              
             </div>
           </li>
           {/*/.portfolio-item*/}
           <li className="portfolio-item logo">
             <div className="item-inner">
               <img
-                src="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
+                src="https://i.ibb.co/vcJHV0Z/Screenshot-116.png"
                 alt=""
               />
-              <h5>Lorem ipsum dolor sit amet</h5>
-              <div className="layer">
-                <a
-                  className="preview view"
-                  href="https://dummyimage.com/600x400/1a1b1c/e4e5f0.png&text=Work"
-                >
-                  <i className="fa fa-eye" />
-                </a>
-              </div>
+               <h5>Get code from <a href="https://youtu.be/tw81OvkhXKY"> youtube description</a></h5>
+              
             </div>
           </li>
           {/*/.portfolio-item*/}
